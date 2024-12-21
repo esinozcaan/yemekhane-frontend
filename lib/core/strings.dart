0891b63f35f515ca0ee13708bf1627eb66336763
Esin <030121039@std.izu.edu.tr>
2024-12-21 23:50:44 +0300

    string.dart güncel

--- lib/core/strings.dart
+++ lib/core/strings.dart
@@ -1,6 +1,5 @@
 class AppStrings {
   static String home = "Anasayfa";
-  static String welcome = "Hoşgeldiniz!";
   static String takePhoto = "Fotoğraf Çek";
   static String selectGallery = "Galeriden Seç";
   static String permissionErrText =
